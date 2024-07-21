42bc6d5bcba98d446c39e0822e447389570d8b88
Nilay Banerjee <n00bie@Nilays-MacBook-Air.local>
2024-07-21 20:06:02 +0530

    Finished Photo Component

--- components/ui/photo.tsx
+++ components/ui/photo.tsx
@@ -5,18 +5,60 @@ import Image from "next/image";
 
 export function Photo() {
   return (
-    <div className="w-full h-full relative">
-      <motion.div>
-        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] ">
+    <div className="w-full h-full relative ">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 1.3, duration: 0.4, ease: "easeIn" },
+        }}
+      >
+        {/* Animated Image  */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 1.7, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute "
+        >
           <Image
-            src="/assets/profile_pic_4.png"
+            src="/assets/img.png"
             priority
             quality={100}
             fill
             alt="Profile Picture"
-            className="object-contain "
+            className="object-contain pt-4"
           />
-        </div>
+        </motion.div>
+
+        {/* Animated Circle */}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
