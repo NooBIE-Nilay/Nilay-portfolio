374f6905f06d9b1b0d7919c849eb24a228538f04
Nilay Banerjee <n00bie@Nilays-MacBook-Air.local>
2024-07-21 23:47:15 +0530

    Added Data for Resume Section

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,3 +1,100 @@
+"use client";
+
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs, SiExpress } from "react-icons/si";
+
+const about = {
+  title: "About me",
+  description:
+    "I'm a 3rd Year Student at KIIT Bhubaneswar, Odisha, majoring in Computer Science and Systems Engineering.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Nilay Banerjee",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+91) 8167588098",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "nbanerjee02.asn@gmail.com",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "Fresher",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Indian",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Hindi, Bengali",
+    },
+  ],
+};
+
+const achievements = {
+  icon: "/assets/resume/badge.svg",
+  title: "My Achievements",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe consequuntur deleniti at minima non nobis quaerat? Dolor",
+  items: [
+    {
+      title: "",
+      description: "",
+      period: "",
+    },
+    {
+      title: "",
+      description: "",
+      period: " ",
+    },
+    {
+      title: "",
+      description: "",
+      period: " ",
+    },
+  ],
+};
+
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My Education",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe consequuntur deleniti at minima non nobis quaerat? Dolor",
+  items: [
+    {
+      institution: "",
+      degree: "",
+      period: "",
+    },
+    {
+      institution: "",
+      degree: "",
+      period: "",
+    },
+    {
+      institution: "",
+      degree: "",
+      period: "",
+    },
+  ],
+};
+
+const skills = {
+  title: "My Skills",
+  description: "",
+};
 export default function Resume() {
-  return <div>Resume Page</div>;
+  return <div></div>;
 }
